47f00ad8e0837d152dffd3fe48fc569476f298d4
Igor C. Brasil <igorbrasilc@hotmail.com>
2022-06-16 15:02:00 -0300

    fix bugs with the schema middleware

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,8 +3,13 @@ import db from '../database.js';
 
 export async function signUp(req, res) {
     const {name, email, password} = req.body;
+    const {body} = req;
 
     try {
+        const userSearch = await db.query('SELECT id FROM users WHERE email = $1', [body.email]);
+
+        if (userSearch.rowCount > 0) return res.status(409).send('Usuário já cadastrado');
+        
         await db.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3)', [name, email, password]);
 
         res.status(201).send('Usuário cadastrado!');
--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,11 +1,11 @@
 /* eslint-disable import/extensions */
 import { Router } from 'express';
-
-import { signUpValidator } from '../middlewares/authMiddleware.js';
+import {validateSchema} from '../middlewares/schemaValidator.js';
+import { signUpSchema } from  '../schemas/authSchemas.js';
 import { signUp } from '../controllers/authController.js';
 
 const authRouter = Router();
 
-authRouter.post('/cadastro', signUpValidator, signUp);
+authRouter.post('/cadastro', validateSchema(signUpSchema), signUp);
 
 export default authRouter;
