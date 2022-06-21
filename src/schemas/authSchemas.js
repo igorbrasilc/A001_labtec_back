d09087096de338a8f79f1ba895edffa4479c984d
Igor C. Brasil <igorbrasilc@hotmail.com>
2022-06-21 14:23:48 -0300

    minor changes

--- src/schemas/authSchemas.js
+++ src/schemas/authSchemas.js
@@ -3,7 +3,7 @@ import joi from 'joi';
 export const signUpSchema = joi.object({
     name: joi.string().required(),
     email: joi.string().email().required(),
-    password: joi.string().regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/).required(),
+    password: joi.string().regex(/^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$/).required(),
     confirmPassword: joi.string().valid(joi.ref('password')).required()
 });
 
