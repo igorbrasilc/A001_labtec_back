ecfb2671a9998795d444beac14b2a70d35de63f3
Igor C. Brasil <igorbrasilc@hotmail.com>
2022-08-16 16:33:20 -0300

    fix issues

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,32 +1,32 @@
-import prisma from "../src/database.js";
-import bcrypt from "bcrypt";
-import "dotenv/config";
-
-const USER_ADMIN = {
-  name: "cesfi",
-  levelId: 1,
-  email: "cesfi@udesc.br",
-  password: bcrypt.hashSync(process.env.USER_ADMIN || "secret", 10),
-};
+import prisma from '../src/database.js';
+import bcrypt from 'bcrypt';
+import 'dotenv/config';
 
 const accessLevels = [
-  {
-    level: "admin",
-  },
-  { level: "user" },
+    {
+        level: 'admin',
+    },
+    { level: 'user' },
 ];
 
+const USER_ADMIN = {
+    name: 'cesfi',
+    levelId: 1,
+    email: 'cesfi@udesc.br',
+    password: bcrypt.hashSync(process.env.USER_ADMIN || 'secret', 10),
+};
+
 async function main() {
-  await prisma.$connect();
-  await prisma.users.create({ data: USER_ADMIN });
-  await prisma.accessLevels.createMany({ data: accessLevels });
+    await prisma.$connect();
+    await prisma.accessLevels.createMany({ data: accessLevels });
+    await prisma.users.create({ data: USER_ADMIN });
 }
 
 main()
-  .catch((err) => {
-    console.log("Error", err);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+    .catch((err) => {
+        console.log('Error', err);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
